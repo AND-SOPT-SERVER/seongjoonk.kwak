713998829466c26fafbfff29619602de66126524
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2024-10-10 12:42:28 +0900

    [chore] 주석 정리

--- src/main/java/org/sopt/seminar1/Validator.java
+++ src/main/java/org/sopt/seminar1/Validator.java
@@ -5,7 +5,9 @@ import java.util.regex.Pattern;
 
 public class Validator {
 
-    //Grapheme Cluster(한 글자로 인식하는 단위)의 정규식, 검색할 패턴을 정의함
+    // Grapheme Cluster(한 글자로 인식하는 단위)의 정규식, 검색할 패턴을 정의함
+    // Grapheme Cluster는 사용자가 하나의 문자로 인식하는 유니코드 문자의 집합.
+    // 결합된 이모지나 음성 기호가 붙은 문자는 여러 개의 코드 포인트로 이루어져 있지만, 이거를 하나로 인식하기 위함.
     private static final Pattern GRAPHEMEPATTERN = Pattern.compile("\\X");
 
     //위에서 만든 Pattern 객체를 통해 특정 문자열을 검색하여 패턴을 찾음
@@ -15,7 +17,7 @@ public class Validator {
     private static final int MAX_DIARY_BODY_COUNT = 30;
 
     // String으로 들어온 id 검증하여 Long으로 출력
-    public static Long validateId(String inputId) {
+    public static Long validateId(final String inputId) {
         try {
             long id = Long.parseLong(inputId);  // String을 long으로 변환
             if (id > 0) {  // ID는 0보다 큰 값이어야 함
@@ -43,11 +45,15 @@ public class Validator {
     }
 
     //이모지 정규식 사용하여 이모지(단일), 이모지(결합)이든 뭐든 한 글자로 인식
-    public static int getLengthOfEmojiContainableText(String text) {
-        if (text == null) {
+    //Java의 String.length()는 문자열이 갖고 있는 유니코드 코드 단위, 즉 UTF-16 코드 유닛의 개수를 반환함 -> 2바이트(하나의 코드유닛)
+    //이모지나 복합 문자(결합된 이모지, 일부 유니코드 문자 등)는 2바이트 이상의 길이를 가짐
+    public static int getLengthOfEmojiContainableText(final String body) {
+        if (body == null) {
             return 0;
         }
-        GRAPHEMEMATCHER.reset(text);
+
+        //주어진 body에서 원하는 정규식 패턴을 찾기 위해 재설정
+        GRAPHEMEMATCHER.reset(body);
         int count = 0;
         while (GRAPHEMEMATCHER.find()) {
             count++;
