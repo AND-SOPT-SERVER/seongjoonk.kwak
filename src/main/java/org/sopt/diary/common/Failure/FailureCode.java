da7291c37ae2c170fd1578b45757afb1982db56d
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2024-10-25 01:45:58 +0900

    [chore] 코드정리

--- src/main/java/org/sopt/diary/common/Failure/FailureCode.java
+++ src/main/java/org/sopt/diary/common/Failure/FailureCode.java
@@ -3,7 +3,6 @@ package org.sopt.diary.common.Failure;
 import org.springframework.http.HttpStatus;
 
 public interface FailureCode {
-    String name();
     HttpStatus getStatus();
     String getMessage();
 }
--- src/main/java/org/sopt/diary/exception/NotFoundException.java
+++ src/main/java/org/sopt/diary/exception/NotFoundException.java
@@ -1,6 +1,5 @@
 package org.sopt.diary.exception;
 
-import org.sopt.diary.common.Failure.DiaryFailureInfo;
 import org.sopt.diary.common.Failure.FailureCode;
 
 public class NotFoundException extends BusinessException{
